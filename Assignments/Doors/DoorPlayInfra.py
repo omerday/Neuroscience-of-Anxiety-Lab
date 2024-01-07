3974045edf45498108f95a1b5a8d6b6eaa9dea6a
Omer <omer.dayan4u@gmail.com>
2024-01-07 10:36:49 +0200

    Added the locked and reduced ITI time

--- Assignments/Doors/DoorPlayInfra.py
+++ Assignments/Doors/DoorPlayInfra.py
@@ -363,12 +363,12 @@ def start_door(window: visual.Window, params, image: visual.ImageStim, reward: i
     else:
         miniDf = pandas.concat([miniDf, pandas.DataFrame.from_records([dict_for_df])])
 
-        # doorFrameImg = visual.ImageStim(window, image=params['doorImagePathPrefix'] + 'lock' + ".png",
-        #                                 size=(image.size[0], image.size[1]),
-        #                                 pos=(0, 0), units="norm", opacity=1)
-        # image.draw()
-        # doorFrameImg.draw()
-        # window.update()
+        doorFrameImg = visual.ImageStim(window, image=params['doorImagePathPrefix'] + 'lock' + ".png",
+                                        size=(image.size[0], image.size[1]),
+                                        pos=(0, 0), units="norm", opacity=1)
+        image.draw()
+        doorFrameImg.draw()
+        window.update()
 
         waitTimeStart = time.time()
         while time.time() < waitTimeStart + WAIT_TIME_ON_DOOR:
@@ -376,7 +376,7 @@ def start_door(window: visual.Window, params, image: visual.ImageStim, reward: i
             Df = pandas.concat([Df, pandas.DataFrame.from_records([dict_for_df])])
             core.wait(1 / 1000)
 
-        # del doorFrameImg
+        del doorFrameImg
         window.update()
 
 
@@ -385,7 +385,7 @@ def start_door(window: visual.Window, params, image: visual.ImageStim, reward: i
     image.draw()
     window.update()
     start_time = time.time()
-    iti_time = 2 + random.random() * 2
+    iti_time = 1 + random.random() * 2
     while time.time() < start_time + iti_time:
         dict_for_df["CurrentTime"] = round(time.time() - dict_for_df['StartTime'], 2)
         dict_for_df["ITI_duration"] = iti_time
