0e0398506874c6ebbf2c71c62e7b23be137d132a
Omer <omer.dayan4u@gmail.com>
2024-05-04 15:23:07 +0300

    Tasks requirements

--- Assignments/TIM/squareRun.py
+++ Assignments/TIM/squareRun.py
@@ -8,6 +8,12 @@ import VAS
 def square_run(window: visual.Window, params: dict, device, io):
     keyboard = io.devices.keyboard
     # TODO: Add pre-ITI
+    """
+    We need to add a method that presents a cross screen for a period of 4-6 seconds.
+    The cross image is located under the ./img folder
+    keep the min and max times as arguments in params.
+    The method will be called here to create a little wait before the presentation of the first square.
+    """
     repeats = params['nTrials'] // len(params['colors'])
     colors_order = []
     for color in params['colors']:
@@ -19,6 +25,13 @@ def square_run(window: visual.Window, params: dict, device, io):
         color_index = params['colors'].index(curr_color)
         temperature = params['temps'][color_index]
         # TODO: Add 2nd paradigm
+        """
+        In the second paradigm, only a one, medium-sized square is presented on the screen for a randomized
+        time of 10-14, following a heat pain.
+        So we need to add an argument in the opening screen to determine whether we want multiple squares or one,
+        and modify this section accordingly with an "if" statement.
+        (Maybe dividing to two functions would be prettier?)
+        """
         for i in range(1, 6):
             display_time = random.uniform(params['squareDurationMin'], params['squareDurationMax'])
             square = visual.ImageStim(window, image=f"./img/squares/{curr_color}_{i}.jpeg", units="norm", size=(2,2))
@@ -39,8 +52,16 @@ def square_run(window: visual.Window, params: dict, device, io):
         if params['painSupport']:
             import heatHandler
             heatHandler.deliver_pain(window, float(temperature), device)
-        # TODO: Add Post ITI
+
         VAS.run_vas(window, io, params, "PainRating", params['painRateDuration'])
+
+        # TODO: Add Post ITI
+        """
+        We need to add a method that presents a blank screen for a period of 7-9 seconds.
+        The cross image is located under the ./img folder
+        keep the min and max times as arguments in params.
+        The method will be called here to create a little wait after the heat rating
+        """
 
 
 def wait_for_time(window:visual.Window, start_time, display_time, keyboard):
