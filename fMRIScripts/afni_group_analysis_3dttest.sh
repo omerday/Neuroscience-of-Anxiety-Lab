925ca883cb525217e3c8a7d78d266384ec961f92
Omer <omer.dayan4u@gmail.com>
2025-01-18 16:40:59 +0200

    group analysis added intersection mask

--- fMRIScripts/afni_group_analysis_3dttest.sh
+++ fMRIScripts/afni_group_analysis_3dttest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sub_a_paths="-setA MDMA"
+sub_a_paths=""
 sub_b_paths=""
+sub_a_argument="-setA MDMA"
+sub_b_argument=""
 prefix="group_analysis_`date +"%H"`.`date +%M`"
 
 if [[ $# -eq 0 ]]; then
@@ -18,20 +20,31 @@ fi
 
 #Loop over subjects in the test group and add their stats to the path
 for subj in `cat subA.txt`; do
-    sub_a_paths="${sub_a_paths} ${subj} ./InputAfni/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
+    sub_a_argument="${sub_a_argument} ${subj} ./InputAfni/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
+    sub_a_paths="${sub_a_paths} ./InputAfni/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
 done
 
 if [ ! -f subB.txt ]; then
     echo "subA.txt not found, Running single group analysis."
 else
-    sub_b_paths="-setB Control"
+    sub_b_argument="-setB Control"
     #Loop over subjects in the control group
     for subj in `cat subB.txt`; do
-    sub_b_paths="${sub_b_paths} ${subj} ./Control/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
+    sub_b_argument="${sub_b_argument} ${subj} ./Control/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
+    sub_b_paths="${sub_b_paths} ./Control/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
     done
 fi
 
+3dmask_tool \
+    -input \
+        ${sub_a_paths} \
+        ${sub_b_paths} \
+    -prefix \
+        ${prefix}_intersect \
+    -frac 0.7
+
 3dttest++ \
-    -prefix prefix \
-    ${sub_a_paths} \
-    ${sub_b_paths}
+    ${sub_a_argument} \
+    ${sub_b_argument} \
+    -prefix ${prefix} \
+    -mask ${prefix}_intersect+tlrc
