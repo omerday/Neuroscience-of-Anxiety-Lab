a66aed3e37bc09c1b8bf973ab6e60edc0a21784d
Omer <omer.dayan4u@gmail.com>
2025-01-18 13:50:50 +0200

    group analysis syntax corrections

--- fMRIScripts/afni_group_analysis_3dttest.sh
+++ fMRIScripts/afni_group_analysis_3dttest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sub_a_paths="-setA"
+sub_a_paths="-setA MDMA"
 sub_b_paths=""
 prefix="group_analysis_`date +"%H"`.`date +%M`"
 
-if ["$#" -e 0]; then
+if [[ $# -eq 0 ]]; then
     echo "No prefix provided, using default"
 else
     prefix=$1
@@ -18,16 +18,17 @@ fi
 
 #Loop over subjects in the test group and add their stats to the path
 for subj in `cat subA.txt`; do
-    sub_a_paths ="${sub_a_paths} ~/Desktop/MDMA/InputAfni/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
+    sub_a_paths="${sub_a_paths} ${subj} ./InputAfni/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
 done
 
 if [ ! -f subB.txt ]; then
     echo "subA.txt not found, Running single group analysis."
 else
-    sub_b_paths="-subB"
+    sub_b_paths="-setB Control"
     #Loop over subjects in the control group
     for subj in `cat subB.txt`; do
-    sub_b_paths ="${sub_b_paths} ~/Desktop/MDMA/Control/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
+    sub_b_paths="${sub_b_paths} ${subj} ./Control/${subj}.results/stats.${subj}+tlrc[neg-neut-blck_GLT#0_Coef]"
+    done
 fi
 
 3dttest++ \
